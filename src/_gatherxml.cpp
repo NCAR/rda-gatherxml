3e445ef939c945e061345e9c54987e82bec61e97
rda-dattore <dattore@ucar.edu>
2020-11-17 09:35:37 -0700

    added threads for 'inventory all'

--- src/_gatherxml.cpp
+++ src/_gatherxml.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <signal.h>
 #include <string>
 #include <regex>
 #include <sstream>
@@ -38,13 +39,16 @@ std::string webhome()
   }
 }
 
+void *do_inventory(void *ts)
+{
+  auto *command=reinterpret_cast<std::string *>(ts);
+  std::stringstream output,error;
+  unixutils::mysystem2(*command,output,error);
+  return nullptr;
+}
+
 void inventory_all()
 {
-  MySQL::LocalQuery query;
-  MySQL::Row row;
-  std::string dsnum=strutils::substitute(metautils::args.dsnum,".","");
-  std::stringstream output,error;
-
   if (metautils::args.data_format != "grib" && metautils::args.data_format != "grib2" && metautils::args.data_format != "grib0" && metautils::args.data_format != "cfnetcdf" && metautils::args.data_format != "hdf5nc4") {
     metautils::log_error("unable to inventory '"+metautils::args.data_format+"' files","gatherxml",USER);
   }
@@ -52,6 +56,8 @@ void inventory_all()
   if (!server) {
     metautils::log_error("unable to connected to RDA metadata database server","gatherxml",USER);
   }
+  std::string dsnum=strutils::substitute(metautils::args.dsnum,".","");
+  MySQL::LocalQuery query;
   if (MySQL::table_exists(server,"IGrML.ds"+dsnum+"_inventory_summary")) {
     query.set("select w.webID,f.format from WGrML.ds"+dsnum+"_webfiles as w left join IGrML.ds"+dsnum+"_inventory_summary as i on i.webID_code = w.code left join WGrML.formats as f on f.code = w.format_code where isnull(i.webID_code) or isnull(inv)");
   }
@@ -68,8 +74,16 @@ void inventory_all()
     metautils::log_error("inventory_all() returned error: '"+query.error()+"'","gatherxml",USER);
   }
   server.disconnect();
+  const size_t MAX_NUM_THREADS=4;
+  auto t_idx=new int[MAX_NUM_THREADS];
+  for (size_t n=0; n < MAX_NUM_THREADS; ++n) {
+    t_idx[n]=-1;
+  }
+  auto *tids=new pthread_t[MAX_NUM_THREADS];
+  auto *commands=new std::string[MAX_NUM_THREADS];
+  int num_threads=0,thread_index=0;
   size_t n=0;
-  while (query.fetch_row(row)) {
+  for (const auto& row : query) {
     std::string format=row[1];
     strutils::replace_all(format,"WMO_","");
     format=strutils::to_lower(format);
@@ -81,12 +95,31 @@ void inventory_all()
     }
     ++n;
     if (format == metautils::args.data_format || (format == "netcdf" && metautils::args.data_format == "cfnetcdf")) {
-	std::string command=metautils::directives.local_root+"/bin/gatherxml";
-	if (n != query.num_rows() && (n % 100) != 0) {
-	  command+=" -R -S";
+	while (num_threads == MAX_NUM_THREADS) {
+	  for (int m=0; m < num_threads; ++m) {
+	    if (pthread_kill(tids[m],0) != 0) {
+		pthread_join(tids[m],nullptr);
+		thread_index=m;
+		t_idx[m]=-1;
+		--num_threads;
+		break;
+	    }
+	  }
 	}
-	command+=" -d "+metautils::args.dsnum+" -f "+metautils::args.data_format+" -I https://rda.ucar.edu"+webhome()+"/"+row[0];
-	unixutils::mysystem2(command,output,error);
+	commands[thread_index]=metautils::directives.local_root+"/bin/gatherxml";
+	if (n != query.num_rows() && (n % 100) != 0) {
+	  commands[thread_index]+=" -R -S";
+	}
+	commands[thread_index]+=" -d "+metautils::args.dsnum+" -f "+metautils::args.data_format+" -I https://rda.ucar.edu"+webhome()+"/"+row[0];
+	pthread_create(&tids[thread_index],nullptr,do_inventory,reinterpret_cast<void *>(&commands[thread_index]));
+	t_idx[thread_index]=0;
+	++thread_index;
+	++num_threads;
+    }
+  }
+  for (size_t n=0; n < MAX_NUM_THREADS; ++n) {
+    if (t_idx[n] == 0) {
+	pthread_join(tids[n],nullptr);
     }
   }
 }
