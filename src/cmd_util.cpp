be044077692a4c719233419d567c744126c6d861
rda-dattore <dattore@ucar.edu>
2021-07-09 11:10:40 -0600

    restyled

--- src/cmd_util.cpp
+++ src/cmd_util.cpp
@@ -1,56 +1,65 @@
 #include <iostream>
-#include <fstream>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <string>
-#include <sstream>
 #include <metadata.hpp>
 #include <MySQL.hpp>
 #include <strutils.hpp>
 #include <utils.hpp>
-#include <myerror.hpp>
+
+using metautils::log_error2;
+using std::endl;
+using std::cerr;
+using std::cout;
+using std::string;
+using std::stringstream;
+using strutils::substitute;
+using unixutils::mysystem2;
 
 metautils::Directives metautils::directives;
 metautils::Args metautils::args;
-std::string myerror="";
-std::string mywarning="";
+string myerror = "";
+string mywarning = "";
 
-int main(int argc,char **argv)
-{
-  metautils::args.args_string=unixutils::unix_args_string(argc,argv,'!');
-  std::string cmd_util=argv[0];
-  auto idx=cmd_util.rfind("/");
-  if (idx != std::string::npos) {
-    cmd_util=cmd_util.substr(idx+1);
+int main(int argc, char **argv) {
+  metautils::args.args_string = unixutils::unix_args_string(argc, argv, '!');
+  string exec = argv[0];
+  auto idx = exec.rfind("/");
+  if (idx != string::npos) {
+    exec = exec.substr(idx + 1);
   }
-  std::string user=getenv("USER");
-  metautils::read_config(cmd_util,user);
-  MySQL::Server server(metautils::directives.database_server,metautils::directives.rdadb_username,metautils::directives.rdadb_password,"dssdb");;
-  if (!server) {
-    metautils::log_error("unable to connect to database; error: "+server.error(),cmd_util,user);
+  string u = getenv("USER");
+  if (!metautils::read_config(exec, u)) {
+    log_error2("configuration error: '" + myerror + "'", "main()", exec, u);
   }
-  MySQL::Query query("stat_flag","dssgrp","logname = '"+user+"'");
-  if (query.submit(server) < 0) {
-     metautils::log_error("authorization server error: "+query.error(),cmd_util,user);
+  MySQL::Server srv(metautils::directives.database_server, metautils::
+      directives.rdadb_username, metautils::directives.rdadb_password, "dssdb");
+  if (!srv) {
+    log_error2("unable to connect to database; error: " + srv.error(), "main()",
+        exec, u);
+  }
+  MySQL::Query q("stat_flag", "dssgrp", "logname = '" + u + "'");
+  if (q.submit(srv) < 0) {
+    log_error2("authorization server error: " + q.error(), "main()", exec, u);
   }
   MySQL::Row row;
-  if (!query.fetch_row(row)) {
-    std::cerr << "Error: not authorized" << std::endl;
-    exit(1);
+  if (!q.fetch_row(row)) {
+    cerr << "Error: not authorized" << endl;
+    return 1;
   }
-  setreuid(15968,15968);
-  std::stringstream output,error;
+  setreuid(15968, 15968);
+  stringstream oss, ess;
   if (!metautils::args.args_string.empty()) {
-    unixutils::mysystem2(metautils::directives.dss_bindir+"/_"+cmd_util+" "+strutils::substitute(metautils::args.args_string,"!"," "),output,error);
+    mysystem2(metautils::directives.decs_bindir + "/_" + exec + " " +
+        substitute(metautils::args.args_string, "!", " "),
+        oss, ess);
+  } else {
+    mysystem2(metautils::directives.decs_bindir + "/_" + exec, oss, ess);
   }
-  else {
-    unixutils::mysystem2(metautils::directives.dss_bindir+"/_"+cmd_util,output,error);
+  if (!oss.str().empty()) {
+    cout << oss.str() << endl;
   }
-  if (!output.str().empty()) {
-    std::cout << output.str() << std::endl;
+  if (!ess.str().empty()) {
+    cerr << ess.str() << endl;
+    return 1;
   }
-  if (!error.str().empty()) {
-    std::cerr << error.str() << std::endl;
-  }
+  return 0;
 }
