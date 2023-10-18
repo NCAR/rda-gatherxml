34a86eea0c55c95e7044b8c3419ef7c691d2cc64
rda-dattore <dattore@ucar.edu>
2023-10-18 10:49:50 -0600

    updates

--- src/libgatherxml/fix_metadata.cpp
+++ src/libgatherxml/fix_metadata.cpp
@@ -5,13 +5,16 @@
 #include <MySQL.hpp>
 #include <bitmap.hpp>
 #include <strutils.hpp>
+#include <utils.hpp>
 #include <search.hpp>
 
 using namespace MySQL;
-using metautils::log_error;
+using metautils::log_error2;
+using miscutils::this_function_label;
 using std::string;
 using std::unordered_map;
 using strutils::split;
+using strutils::strand;
 using strutils::substitute;
 
 namespace gatherxml {
@@ -19,35 +22,31 @@ namespace gatherxml {
 namespace summarizeMetadata {
 
 void summarize_fix_data(string caller, string user) {
-  const string THIS_FUNC = __func__;
+  static const string F = this_function_label(__func__);
   string dsnum2 = substitute(metautils::args.dsnum, ".", "");
   Server server(metautils::directives.database_server, metautils::directives.
       metadb_username, metautils::directives.metadb_password, "");
   Query formats_query("code, format_code", "WFixML.ds" + dsnum2 + "_webfiles2");
   if (formats_query.submit(server) < 0) {
-    log_error(THIS_FUNC + "(): " + formats_query.error(), caller, user);
+    log_error2(formats_query.error(), F, caller, user);
   }
   unordered_map<string, string> data_file_formats_map;
   for (const auto& format_row : formats_query) {
     data_file_formats_map.emplace(format_row[0], format_row[1]);
   }
   string error;
-  if (server.command("lock tables WFixML.ds" + dsnum2 + "_locations write",
-      error) < 0) {
-    log_error(THIS_FUNC + "(): " + server.error(), caller, user);
-  }
   LocalQuery locations_query("file_code, classification_code, start_date, "
       "end_date, box1d_row, box1d_bitmap", "WFixML.ds" + dsnum2 + "_locations");
   if (locations_query.submit(server) < 0) {
-    log_error(THIS_FUNC + "(): " + locations_query.error(), caller, user);
+    log_error2(locations_query.error(), F, caller, user);
   }
   my::map<SummaryEntry> summary_table;
   for (const auto& location_row : locations_query) {
     if (data_file_formats_map.find(location_row[0]) == data_file_formats_map.
         end()) {
-      log_error(THIS_FUNC + "() found a webID (" + location_row[0] + ") in "
-          "WFixML.ds" + dsnum2 + "_locations that doesn't exist in WFixML.ds" +
-          dsnum2 + "_webfiles2", caller, user);
+      log_error2("found a webID (" + location_row[0] + ") in WFixML.ds" + dsnum2
+          + "_locations that doesn't exist in WFixML.ds" + dsnum2 +
+          "_webfiles2", F, caller, user);
     }
     SummaryEntry se;
     se.key = data_file_formats_map[location_row[0]] + "<!>" + location_row[1] +
@@ -71,33 +70,31 @@ void summarize_fix_data(string caller, string user) {
       summary_table.replace(se);
     }
   }
-  if (server.command("unlock tables", error) < 0) {
-    log_error(THIS_FUNC + "(): " + server.error(), caller, user);
-  }
-  if (server.command("lock tables search.fix_data write", error) < 0) {
-    log_error(THIS_FUNC + "(): " + server.error(), caller, user);
-  }
-  server._delete("search.fix_data", "dsid = '" + metautils::args.dsnum + "'");
+  auto uflg = strand(3);
   for (const auto& key : summary_table.keys()) {
     auto parts = split(key, "<!>");
     SummaryEntry se;
     summary_table.found(key, se);
-    if (server.insert("search.fix_data", "'" + metautils::args.dsnum + "', " +
-        parts[0] + ", " + parts[1] + ", '" + se.start_date + "', '" + se.
-        end_date + "', " + parts[2] + ", '" + se.box1d_bitmap + "'") < 0) {
-      error = server.error();
-      if (error.find("Duplicate entry") != 0) {
-        log_error(THIS_FUNC + "(): " + error, caller, user);
+    if (server.insert(
+          "search.fix_data",
+          "dsid, format_code, classification_code, start_date, end_date, "
+              "box1d_row, box1d_bitmap, uflg",
+          "'" + metautils::args.dsnum + "', " + parts[0] + ", " + parts[1] +
+              ", '" + se.start_date + "', '" + se.  end_date + "', " + parts[2]
+              + ", '" + se.box1d_bitmap + "', '" + uflg + "'",
+          "update uflg = values(uflg)"
+          ) < 0) {
+      if (server.error().find("Duplicate entry") != 0) {
+        log_error2(server.error(), F, caller, user);
       }
     }
   }
-  if (server.command("unlock tables", error) < 0) {
-    log_error(THIS_FUNC + "(): " + server.error(), caller, user);
-  }
+  server._delete("search.fix_data", "dsid = '" + metautils::args.dsnum + "' "
+      "and uflg != '" + uflg + "'");
   summarize_locations("WFixML", error);
   if (!error.empty()) {
-    log_error(THIS_FUNC + "(): summarize_locations() returned '" + error + "'",
-        caller, user);
+    log_error2("summarize_locations() returned '" + error + "'", F, caller,
+        user);
   }
   server.disconnect();
 }
