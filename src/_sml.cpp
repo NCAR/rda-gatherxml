58ea3aea7dfc4583335a843b9e38c2f798ab0d81
rda-dattore <dattore@ucar.edu>
2024-09-12 17:04:12 -0600

    update for new dataset IDs

--- src/_sml.cpp
+++ src/_sml.cpp
@@ -11,6 +11,9 @@
 #include <myerror.hpp>
 
 using namespace PostgreSQL;
+using strutils::ds_aliases;
+using strutils::ng_gdex_id;
+using strutils::to_sql_tuple_string;
 
 metautils::Directives metautils::directives;
 metautils::Args metautils::args;
@@ -19,7 +22,6 @@ std::string mywarning="";
 
 Server server_m,server_d;
 std::string user=getenv("USER");
-std::string dsnum2;
 std::string ds_meta_link="N";
 struct Entry {
   Entry() : key(),meta_link() {}
@@ -29,6 +31,7 @@ struct Entry {
 };
 my::map<Entry> files_with_metadata;
 std::list<std::string> file_list;
+std::string g_ds_set;
 
 void fill_files_with_metadata(std::string filename = "")
 {
@@ -43,12 +46,12 @@ void fill_files_with_metadata(std::string filename = "")
     if (dbs[n][0] == 'W') {
 	meta_link.assign(&dbs[n][1],2);
     }
-    if (table_exists(server_m,std::string(dbs[n])+".ds"+dsnum2+"_webfiles2")) {
+    if (table_exists(server_m,std::string(dbs[n])+"."+metautils::args.dsid+"_webfiles2")) {
 	if (!filename.empty()) {
-	  query.set("select id from "+dbs[n]+".ds"+dsnum2+"_webfiles2"+" where id = '"+filename+"'");
+	  query.set("select id from "+dbs[n]+"."+metautils::args.dsid+"_webfiles2"+" where id = '"+filename+"'");
 	}
 	else {
-	  query.set("select id from "+dbs[n]+".ds"+dsnum2+"_webfiles2");
+	  query.set("select id from "+dbs[n]+"."+metautils::args.dsid+"_webfiles2");
 	}
 	if (query.submit(server_m) < 0) {
 	  metautils::log_error("error '"+query.error()+"' while trying to get "+dbs[n]+" filelist","sml",user);
@@ -64,14 +67,14 @@ void fill_files_with_metadata(std::string filename = "")
 
 void nullify_meta_link(std::string table,std::string file,std::string old_meta_link)
 {
-  if (server_d.update("dssdb."+table,"meta_link = NULL","dsid = 'ds"+metautils::args.dsnum+"' and "+table+" = '"+file+"'") < 0)
+  if (server_d.update("dssdb."+table,"meta_link = NULL","dsid in "+g_ds_set+" and "+table+" = '"+file+"'") < 0)
     metautils::log_error("error '"+server_d.error()+"' while trying to nullify meta_link for '"+file+"'","sml",user);
   std::cout << "Info: changed 'meta_link' from '" << old_meta_link << "' to NULL for file: '" << file << "'" << std::endl;
 }
 
 void update_meta_link(std::string table,std::string file,std::string new_meta_link,std::string old_meta_link)
 {
-  if (server_d.update("dssdb."+table,"meta_link = '"+new_meta_link+"'","dsid = 'ds"+metautils::args.dsnum+"' and "+table+" = '"+file+"'") < 0)
+  if (server_d.update("dssdb."+table,"meta_link = '"+new_meta_link+"'","dsid in "+g_ds_set+" and "+table+" = '"+file+"'") < 0)
     metautils::log_error("error '"+server_d.error()+"' while trying to set meta_link for '"+file+"' to '"+new_meta_link+"'","sml",user);
   std::cout << "Info: changed 'meta_link' from '" << old_meta_link << "' to '" << new_meta_link << "' for file: '" << file << "'" << std::endl;
 }
@@ -82,8 +85,7 @@ void set_file_meta_links()
   Row row;
   Entry e;
 
-//  query.set("select mssfile,meta_link,'mssfile' from mssfile where dsid = 'ds"+metautils::args.dsnum+"' and property = 'P' and retention_days > 0 union select wfile,meta_link,'wfile' from wfile where dsid = 'ds"+metautils::args.dsnum+"' and type = 'D' and property = 'A'");
-query.set("select mssfile,meta_link,'mssfile' from mssfile where dsid = 'ds"+metautils::args.dsnum+"' and type = 'P' and status = 'P' union select wfile,meta_link,'wfile' from wfile where dsid = 'ds"+metautils::args.dsnum+"' and type = 'D' and status = 'P'");
+  query.set("select mssfile,meta_link,'mssfile' from mssfile where dsid in "+g_ds_set+" and type = 'P' and status = 'P' union select wfile,meta_link,'wfile' from wfile where dsid in "+g_ds_set+" and type = 'D' and status = 'P'");
   if (query.submit(server_d) < 0)
     metautils::log_error("error '"+query.error()+"' while trying to get dssdb filelist","sml",user);
   while (query.fetch_row(row)) {
@@ -102,10 +104,10 @@ void set_file_meta_link(std::string filename)
 {
   LocalQuery query;
   if (strutils::has_beginning(filename,"/FS/DECS/")) {
-    query.set("select meta_link,'mssfile' from mssfile where dsid = 'ds"+metautils::args.dsnum+"' and mssfile = '"+filename+"'");
+    query.set("select meta_link,'mssfile' from mssfile where dsid in "+g_ds_set+" and mssfile = '"+filename+"'");
   }
   else {
-    query.set("select meta_link,'wfile' from wfile where dsid = 'ds"+metautils::args.dsnum+"' and wfile = '"+filename+"'");
+    query.set("select meta_link,'wfile' from wfile where dsid in "+g_ds_set+" and wfile = '"+filename+"'");
   }
   if (query.submit(server_d) < 0) {
     metautils::log_error("error '"+query.error()+"' while trying to get entry for '"+filename+"' from dssdb","sml",user);
@@ -140,20 +142,17 @@ int main(int argc,char **argv)
   auto unix_args=strutils::split(metautils::args.args_string,"%");
   for (size_t n=0; n < unix_args.size(); ++n) {
     if (unix_args[n] == "-d") {
-	metautils::args.dsnum=unix_args[++n];
-	if (strutils::has_beginning(metautils::args.dsnum,"ds")) {
-	  metautils::args.dsnum=metautils::args.dsnum.substr(2);
-	}
+	metautils::args.dsid=ng_gdex_id(unix_args[++n]);
+      g_ds_set = to_sql_tuple_string(ds_aliases(metautils::args.dsid));
     }
     else if (n > 1) {
 	file_list.push_back(unix_args[n]);
     }
   }
-  if (metautils::args.dsnum.empty()) {
-    std::cerr << "Error: no dataset number specified" << std::endl;
+  if (metautils::args.dsid.empty()) {
+    std::cerr << "Error: no or invalid dataset ID specified" << std::endl;
     exit(1);
   }
-  dsnum2=strutils::substitute(metautils::args.dsnum,".","");
   metautils::read_config("sml",user,false);
   server_m.connect(metautils::directives.database_server,metautils::directives.metadb_username,metautils::directives.metadb_password,"rdadb");
   server_d.connect(metautils::directives.database_server,metautils::directives.rdadb_username,metautils::directives.rdadb_password,"rdadb");
@@ -170,7 +169,7 @@ int main(int argc,char **argv)
   if (files_with_metadata.size() > 0) {
     ds_meta_link="Y";
   }
-  if (server_d.update("dssdb.dataset","meta_link = '"+ds_meta_link+"', version = version + 1","dsid = 'ds"+metautils::args.dsnum+"'") < 0) {
+  if (server_d.update("dssdb.dataset","meta_link = '"+ds_meta_link+"', version = version + 1","dsid in "+g_ds_set) < 0) {
     metautils::log_error("error '"+server_d.error()+"' while updating meta_link field for dataset","sml",user);
   }
   server_d.disconnect();
