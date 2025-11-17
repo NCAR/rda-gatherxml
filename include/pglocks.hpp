da909c77b2be69cef819e4f8811e0fadea3755ba
rda-dattore <dattore@ucar.edu>
2025-11-17 15:32:51 -0700

    fix lock ID for agrids_cache

--- include/pglocks.hpp
+++ include/pglocks.hpp
@@ -10,7 +10,7 @@ namespace pglocks {
 
 std::unordered_map<std::string, long long> pglocks{
     { "WGrML.summary", 0x4000000000000000 },
-    { "WGrML.dsnnnn_agrids_cache", 0x2000000000000000 }
+    { "WGrML.dnnnnnn_agrids_cache", 0x2000000000000000 }
 };
 
 } // end namespace pglocks
