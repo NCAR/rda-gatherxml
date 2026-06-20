977f96115ec365a4da807b00302535633c14bc94
rda-dattore <dattore@ucar.edu>
2026-06-20 13:06:33 -0600

    removed debug lines

--- src/_obs2xml.cpp
+++ src/_obs2xml.cpp
@@ -1239,9 +1239,7 @@ int main(int argc, char **argv) {
           "obs2xml", USER);
     }
   }
-metautils::log_info("A", "obs2xml", USER);
   if (metautils::args.update_db) {
-metautils::log_info("B", "obs2xml", USER);
     if (!metautils::args.update_graphics) {
       flags = "-G " + flags;
     }
